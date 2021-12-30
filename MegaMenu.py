07dc4a9466c39a55396345e25780ead1349b43c8
Py <79402324+payidarofficial@users.noreply.github.com>
2021-12-30 18:03:31 +0300

    Add files via upload

---
+++ MegaMenu.py
@@ -0,0 +1,48 @@
+# Pâyidar
+
+from tkinter import *
+from tkinter import ttk
+import tkinter
+
+
+def donothing():
+    filewin = Toplevel(root)
+    button = Button(filewin, text="Hiç Bir Şey Yapma")
+    button.pack()
+
+root = Tk()
+root.title("Payidar Code")
+
+menubar = Menu(root)
+filemenu = Menu(menubar, tearoff=0)
+# Menu Element
+filemenu.add_command(label="New", command=donothing)
+filemenu.add_command(label="Open", command=donothing)
+filemenu.add_command(label="Save", command=donothing)
+filemenu.add_command(label="Save As...", command=donothing)
+filemenu.add_command(label="Close", command=donothing)
+
+filemenu.add_separator()
+
+filemenu.add_command(label="Exit", command=root.quit)
+menubar.add_cascade(label="File", menu=filemenu)
+editmenu = Menu(menubar, tearoff=0)
+editmenu.add_command(label="Undo", command=donothing)
+
+editmenu.add_separator()
+
+editmenu.add_command(label="Cut", command=donothing)
+editmenu.add_command(label="Copy", command=donothing)
+editmenu.add_command(label="Paste", command=donothing)
+editmenu.add_command(label="Delete", command=donothing)
+editmenu.add_command(label="Select All", command=donothing)
+
+menubar.add_cascade(label="Edit", menu=editmenu)
+
+helpmenu = Menu(menubar, tearoff=0)
+helpmenu.add_command(label="Help İndex", command=donothing)
+helpmenu.add_command(label="About...", command=donothing)
+menubar.add_cascade(label="Help", menu=helpmenu)
+
+root.config(menu=menubar)
+root.mainloop()
